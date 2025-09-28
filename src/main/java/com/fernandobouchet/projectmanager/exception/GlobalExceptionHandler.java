f1a0a0574e6077fa482b7cab5a84608b2e4a6b8e
Fernando Bouchet <fernandobouchet@gmail.com>
2025-09-28 16:55:22 -0300

    feat: add 'UnauthorizedException'

--- src/main/java/com/fernandobouchet/projectmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fernandobouchet/projectmanager/exception/GlobalExceptionHandler.java
@@ -34,4 +34,9 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
 }
---
+++ src/main/java/com/fernandobouchet/projectmanager/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.fernandobouchet.projectmanager.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
